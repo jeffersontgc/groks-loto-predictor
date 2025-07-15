dab21dee43f21d42f956d714755c1e28c30ec286
Jefferson Irigoyen <jeffersonirigoyen@gmail.com>
2025-07-14 22:51:30 -0600

    ajust done

--- lib/apollo.tsx
+++ lib/apollo.tsx
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const httpLink = new HttpLink({
-  uri: "http://localhost:3000/graphql",
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_URL || "http://localhost:3000/graphql",
 });
 
 const client = new ApolloClient({
